19299e514550d6b67e81269a2445f94c2413e2b1
Jeremy Nagahama <naga@hawaii.edu>
2018-06-03 13:00:51 -1000

    Cleanup History.jsx Table via Boostrap

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,36 +1,59 @@
 import React from "react";
+import { Table } from "react-bootstrap";
 
-const History = data => {
-	const rows = data.data.map(element => (
-		<div>
-			<td align="center" colSpan="2" style={{ textAlign: "center" }}>
-				{element.createDate}
-			</td>
-			<td align="center" colSpan="2" style={{ textAlign: "center" }}>
-				{element.value}
-			</td>
-			<td align="center" colSpan="2" style={{ textAlign: "center" }}>
-				{element.category}
-			</td>
-			<td align="center" colSpan="2" style={{ textAlign: "center" }}>
-				{element.value}
-			</td>
-		</div>
-	));
+const style = {
+	margin: "15px",
+	borderStyle: "ridge",
+	borderWidth: "5px",
+	borderColor: "red",
+};
+
+const History = input => {
+	const data = input.data;
+
+	function createTable() {
+		let table = [];
+
+		//Outer loop
+		for (let i = 0; i < data.length; i++) {
+			let children = [];
+			// Converts category number into a string
+			let category = "";
+			switch (data[i].category) {
+				case 0:
+					category = "Body";
+					break;
+				case 1:
+					category = "Mind";
+					break;
+				case 2:
+					category = "Spirit";
+					break;
+			}
+
+			children.push(<td>{data[i].createDate}</td>);
+			children.push(<td>{data[i].todo}</td>);
+			children.push(<td>{category}</td>);
+			children.push(<td>{data[i].value}</td>);
+			table.push(<tr>{children}</tr>);
+		}
+		return table;
+	}
+
 	return (
-		<table id="history-table">
-			<thead>
-				<tr>
-					<th>Date</th>
-					<th>Todo</th>
-					<th>Category</th>
-					<th>Value</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>{rows}</tr>
-			</tbody>
-		</table>
+		<div style={style}>
+			<Table id="history-table">
+				<thead>
+					<tr>
+						<th>Date</th>
+						<th>Todo</th>
+						<th>Category</th>
+						<th>Value</th>
+					</tr>
+				</thead>
+				<tbody>{createTable()}</tbody>
+			</Table>
+		</div>
 	);
 };
 
